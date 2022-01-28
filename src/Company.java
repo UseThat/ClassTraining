152890f80ff05b42129b3aac36fc62aa4bc775fa
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-01-28 07:49:56 +0100

    Add logic to class

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,15 @@
 public class Company {
+    public static void main(String[] args) {
+        Employee[] empl = new Employee[3];
+
+        empl[0] = new Employee("Mateusz", "Wygoda", 1990, 4);
+        empl[1] = new Employee("Marek", "Wygoda", 1994, 2);
+        empl[2] = new Employee("Wiesiek", "Wygoda", 1996, 6);
+
+        for (Employee obj :
+                empl) {
+            obj.printInformation();
+        }
+
+    }
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,18 @@
 public class Employee {
+    String firstName;
+    String lastName;
+    int birthYear;
+    int seniority;
+
+    public Employee(String firstName, String lastName, int birthYear, int seniority){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.seniority = seniority;
+
+    }
+
+    public void printInformation(){
+        System.out.println("First name: "+firstName+" last name: "+lastName+" birth year: "+birthYear+" company seniority: "+seniority);
+    }
 }
